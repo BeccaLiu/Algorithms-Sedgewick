65cb33fecc1f1a260efd92377785ec9f7b49cb1e
BeccaLiu <rliu@enphaseenergy.com>
2016-09-01 21:23:11 -0700

    test

---
+++ C1_Fundamentals/P3_BagQueueStack/DoubleNode.java
@@ -0,0 +1,23 @@
+package P3_BagQueueStack;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by rliu on 9/1/16.
+ * Exercise 1.3.31
+ */
+public class DoubleNode<Item> {
+    Item first;
+    int size;
+
+    public DoubleNode() {
+        first = null;
+        size = 0;
+    }
+
+    public static void main(String[] args) {
+        StdOut.print("Test");
+        StdOut.print();
+        DoubleNode<Integer> s = new DoubleNode<>();
+    }
+}
