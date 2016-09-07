08b7aa6a88f24f32095c3b2b4d63403faa4700c5
BeccaLiu <rliu@enphaseenergy.com>
2016-09-07 12:26:05 -0700

    web exercise 1 using queue

--- C1_Fundamentals/P3_BagQueueStack/W1_Tail.java
+++ C1_Fundamentals/P3_BagQueueStack/W1_Tail.java
@@ -1,6 +1,7 @@
 package P3_BagQueueStack;
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -10,11 +11,12 @@ import java.io.File;
  * Created by rliu on 9/7/16.
  * Write a program Tail so that Tail k < file.txt prints the last k lines of the file file.txt. Use StdIn.readLine(). Which data structure should you use?
  * http://algs4.cs.princeton.edu/13stacks/
+ * take the hint of web exercise 7, we can also using queue.
  */
 public class W1_Tail {
     public static void main(String[] args) {
         In in = new In(new File("TestData/amendments.txt"));
-        Stack<String> s = new Stack<>();
+        Stack<String> s = new Stack<>(); //using stack will print the last k lines backwards
         while (!in.isEmpty()) {
             s.push(in.readLine());
         }
@@ -22,6 +24,22 @@ public class W1_Tail {
         while (i < 10) {
             StdOut.println(s.pop());
             i++;
+        }
+
+        queueImplementation();
+    }
+
+    public static void queueImplementation() { //using queue will print the last k lines in order
+        In in = new In(new File("TestData/amendments.txt"));
+        Queue<String> q = new Queue<String>();
+        while (!in.isEmpty()) {
+            q.enqueue(in.readLine());
+            if (q.size() == 10)
+                q.dequeue();
+        }
+
+        while (!q.isEmpty()) {
+            StdOut.println(q.dequeue());
         }
     }
 }
