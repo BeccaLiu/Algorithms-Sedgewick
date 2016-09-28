8f2c8234d3ae8b064f38d55bd6dd40852cc8982e
RebeccaLiu <rebecca.liu.usc@gmail.com>
2016-09-27 22:31:15 -0700

    testData generator

--- C1_Fundamentals/P4_AnalysisAlgorithms/TestDataGenerator.java
+++ C1_Fundamentals/P4_AnalysisAlgorithms/TestDataGenerator.java
@@ -6,11 +6,22 @@ import edu.princeton.cs.algs4.StdRandom;
 /**
  * Created by rliu on 9/27/16.
  */
-public class NumForSumProb {
+public class TestDataGenerator {
     public static void main(String[] args) {
+        closestPairOneDimension();
+    }
+
+    public static void intForXSumProb() {
         Out out = new Out("TestData/sumNumber.txt");
         for (int i = 0; i < 100; i++) {
             out.println(StdRandom.uniform(-50, 50));
+        }
+    }
+
+    public static void closestPairOneDimension() {
+        Out out = new Out("TestData/closetPair.txt");
+        for (int i = 0; i < 100; i++) {
+            out.println(StdRandom.uniform(-50.0, 50.0));
         }
     }
 }
