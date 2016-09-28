64141d5c847f9735a0ec06a54563ca0f3ae9df0e
RebeccaLiu <rebecca.liu.usc@gmail.com>
2016-09-27 22:18:11 -0700

    ex 1.4.15 quadratic time 3 sum

--- C1_Fundamentals/P4_AnalysisAlgorithms/E15_3SumFaster.java
+++ C1_Fundamentals/P4_AnalysisAlgorithms/E15_3SumFaster.java
@@ -16,9 +16,15 @@ public class E15_3SumFaster {
         int N = arr.length;
         int cnt = 0;
         for (int i = 0; i < N - 2; i++) {
+            if (i > 0 && arr[i] == arr[i - 1])
+                continue;
             int m = i + 1;
             int n = N - 1;
             while (m < n) {
+                if (m > i + 1 && arr[m] == arr[m - 1]) {
+                    m++;
+                    continue;
+                }
                 if (arr[i] + arr[m] + arr[n] > 0)
                     n--;
                 else if (arr[i] + arr[m] + arr[n] < 0)
@@ -27,7 +33,7 @@ public class E15_3SumFaster {
                     StdOut.println(arr[i] + " " + arr[m] + " " + arr[n]);
                     cnt++;
                     m++;
-                    n++;
+                    n--;
                 }
             }
         }
