a4a8f6f74a4b31df89cc88a62e96b449073ce59e
BeccaLiu <rliu@enphaseenergy.com>
2016-10-08 17:04:42 -0700

    1.5.18 random grid generator update for 1.5.19

--- C1_Fundamentals/P5_UnionFind/E18_RandomGrid.java
+++ C1_Fundamentals/P5_UnionFind/E18_RandomGrid.java
@@ -2,6 +2,8 @@ package P5_UnionFind;
 
 import P3_BagQueueStack.RandomBag;
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.UF;
 
 /**
  * Created by rliu on 10/8/16.
@@ -11,13 +13,16 @@ public class E18_RandomGrid {
     public static void main(String[] args) {
         int N = 50;
         RandomBag<Connection> a = generated(N);
+        UF uf = new UF((N + 1) * (N + 1));//E 1.5.19
         StdDraw.setCanvasSize(400, 400);
         StdDraw.setXscale(0, N);
         StdDraw.setYscale(0, N);
         StdDraw.setPenRadius(0.001);
         StdDraw.setPenColor(StdDraw.BLUE);
         for (Connection c : a) {
+            uf.union(c.p, c.q);
             StdDraw.line(c.p / (N + 1), c.p % (N + 1), c.q / (N + 1), c.q % (N + 1));
+            StdOut.println(uf.count()); //will only print 1 if the grid is fully connected
         }
     }
 
