04d6f4cb39f55c311a5c5214be6bdb72a7ebcffe
BeccaLiu <rliu@enphaseenergy.com>
2016-10-11 22:10:10 -0700

    2.1.13

---
+++ C2_Sorting/P1_ElementarySorts/E13_Card.java
@@ -0,0 +1,90 @@
+package P1_ElementarySorts;
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by rliu on 10/11/16.
+ * Using insertion sort
+ */
+public class E13_Card {
+
+
+    public static void main(String[] args) {
+        ArrayList<Card> cards = new ArrayList<>();
+        for (Suits s : Suits.values())
+            for (Numerals n : Numerals.values())
+                cards.add(new Card(s, n));
+
+        Collections.shuffle(cards);
+        for (Card c : cards)
+            StdOut.print(c + " ");
+        Card[] toSort = cards.toArray(new Card[cards.size()]);
+
+        StdOut.println();
+        for (int i = 1; i < toSort.length; i++) {
+            for (int j = i; j > 0 && toSort[j].compareTo(toSort[j - 1]) < 0; j--) {
+                Card temp = toSort[j];
+                toSort[j] = toSort[j - 1];
+                toSort[j - 1] = temp;
+            }
+        }
+
+        for (Card c : toSort)
+            StdOut.print(c + " ");
+
+    }
+
+    private enum Numerals {
+        ACE(1), DEUCE(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9),
+        TEN(10), JACK(11), QUEEN(12), KING(13);
+
+        int value;
+
+        Numerals(int i) {
+            this.value = i;
+        }
+
+    }
+
+    private enum Suits {
+
+        Spades(1),
+        Hearts(2),
+        Diamonds(3),
+        Clubs(4);
+
+        int value;
+
+        Suits(int i) {
+            this.value = i;
+        }
+    }
+
+    private static class Card implements Comparable<Card> {
+        Suits suit;
+        Numerals rank;
+
+        public Card(Suits suit, Numerals ranks) {
+            this.suit = suit;
+            this.rank = ranks;
+        }
+
+        @Override
+        public int compareTo(Card o) {
+
+            if (this.suit.equals(o.suit)) {
+                return this.rank.compareTo(o.rank);
+            } else {
+                return this.suit.compareTo(o.suit);
+            }
+        }
+
+        public String toString() {
+            return this.suit + "" + this.rank;
+        }
+    }
+}
+
