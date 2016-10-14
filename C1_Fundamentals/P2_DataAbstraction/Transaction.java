c09ed7f8a52a136b45da46f0422ec35b756293f2
BeccaLiu <rliu@enphaseenergy.com>
2016-10-13 21:31:44 -0700

    2.1.22 transaction sorting client

--- C1_Fundamentals/P2_DataAbstraction/Transaction.java
+++ C1_Fundamentals/P2_DataAbstraction/Transaction.java
@@ -1,6 +1,8 @@
 package P2_DataAbstraction;
 
+import P1_ElementarySorts.Shell;
 import edu.princeton.cs.algs4.Date;
+import edu.princeton.cs.algs4.StdOut;
 
 /**
  * Created by rliu on 8/27/16.
@@ -16,6 +18,17 @@ public class Transaction implements Comparable<Transaction> {
         this.who = who;
         this.when = date;
         this.amount = amount;
+    }
+
+    public static void main(String[] args) {
+        Transaction t1 = new Transaction("Mary", new Date("1/1/2015"), 1000.0);
+        Transaction t2 = new Transaction("Bob", new Date(9, 18, 2015), 10.0);
+        Transaction t3 = new Transaction("Sunny", new Date(9, 10, 2011), 200.0);
+        Transaction[] transactions = {t1, t2, t3};
+        Shell.sort(transactions);
+        for (Transaction t : transactions) {
+            StdOut.println(t);
+        }
     }
 
     public boolean equals(Object other) {
